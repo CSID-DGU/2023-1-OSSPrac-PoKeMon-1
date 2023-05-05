92b9ac1049d6bd02853354653dfd502ecd800b84
Hankyul Min <denirokr22@gmail.com>
2023-05-05 13:38:24 +0900

    [2023.05.05 hankyul] (add) main.py에 체크한 행 삭제 기능 추가

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask, redirect,render_template,request
+from flask import Flask, redirect, render_template, request
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+data = dict()
 
 
+@app.route("/delete", methods=["POST", "GET"])
+def delete():
+    if request.method == "POST":
+        studentNumber = request.form.getlist("studentNumber")
+        for studentNumber in studentNumber:
+            del data[int(studentNumber)]
+        result = [data[key] for key in sorted(data)]
+        return render_template("result.html", result=result)
+    return redirect("/result")
 
 
-if __name__=='__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
